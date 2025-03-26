1071bc6a3616aa6b72726595069f20292b0386dd
Artyom <net775@yandex.ru>
2025-03-26 17:11:41 +0300

    Выполнена автоматизация по добавления новых компонентов на портал через React

---
+++ src/componenents/Example.jsx
@@ -0,0 +1,13 @@
+const Example = () => {
+    return (
+        <>
+            <h1>
+                Hello World<br/>
+                <mark>Test</mark>
+            </h1>
+            <h2>Test h2</h2>
+        </>
+    )
+}
+
+export default Example
