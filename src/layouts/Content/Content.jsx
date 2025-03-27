60b667161289cd3d32b2e20bb1b9d8a8ef9d3cf0
artemusenko <ayshenko@gmail.com>
2025-03-27 15:29:12 +0300

    Произведена настройка глобальной обертки для трех страниц

---
+++ src/layouts/Content/Content.jsx
@@ -0,0 +1,7 @@
+import "./Content.scss"
+
+const Content = () => {
+    return <main className="content">Заглушка Content</main>
+}
+
+export default Content
---
+++ src/layouts/Footer/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+    return <footer>Заглушка Footer</footer>
+}
+
+export default Footer
